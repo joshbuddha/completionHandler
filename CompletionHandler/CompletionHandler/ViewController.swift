5223e05511ef79e185aac459b6703f7301a6abd6
Josh Kneedler <ranger@Joshs-MBP.domain>
2018-12-21 14:23:44 -0800

    escaping first commit

---
+++ CompletionHandler/CompletionHandler/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CompletionHandler
+//
+//  Created by Josh Kneedler on 12/21/18.
+//  Copyright © 2018 Josh Kneedler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("escape")
+    }
+
+
+}
+
